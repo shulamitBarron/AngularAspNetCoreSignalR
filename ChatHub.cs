6d8d1130d5150b98bd189a96aa31f80e5d242366
TamarBraverman <t0583224053@gmail.com>
2018-11-04 17:07:50 +0200

    njjk

---
+++ ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
+
+namespace AngularAspNetCoreSignalR
+{
+    public class ChatHub : Hub
+    {
+        public void SendToAll(string name, string message)
+        {
+            Clients.All.InvokeAsync("sendToAll", name, message);
+        }
+        public void RunDeleteProgram(string result)
+        {
+
+            var process = Process.Start("../../Release/DeleteDoubleFiles.exe");
+            if (process == null) // failed to start../
+            {
+                Clients.All.InvokeAsync("runDeleteProgram", "Fail");
+            }
+            else // Started, wait for it to finish
+            {
+                process.WaitForExit();
+                Clients.All.InvokeAsync("runDeleteProgram", "Ok");
+            }
+        }
+    }
+}
