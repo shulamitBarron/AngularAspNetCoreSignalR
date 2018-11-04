4999db939372fc54dcb3a73c21ef16a91d22168d
Shulamit Barron <barrons@amdocs.com>
2018-11-04 22:18:14 +0200

    download exe file

--- RunProgramController.cs
+++ RunProgramController.cs
@@ -8,51 +8,34 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace AngularAspNetCoreSignalR
-{
-    [Route("api/[controller]")]
-    public class RunProgramController : Controller
-    {
+namespace AngularAspNetCoreSignalR {
+    [Route ("api/[controller]")]
+    public class RunProgramController : Controller {
         // GET: api/<controller>
         [HttpGet]
-        public string Get()
-        {
-            var process = Process.Start(Path.Combine(Environment.CurrentDirectory, "Release", "DeleteDoubleFiles.exe")); //Process.Start("/Release/DeleteDoubleFiles.exe");
-            if (process == null) // failed to start../
-            {
-                return "Fail";
-            }
-            else // Started, wait for it to finish
-            {
-                //process.WaitForExit();
-                //process.Start();
-                return "Ok";
-            }
+        public FileResult Get () {
+            var fileName = "DeleteDoubleFiles.exe";
+            var filepath = Path.Combine (Environment.CurrentDirectory, "Release", "DeleteDoubleFiles.exe");
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filepath);
+            return File (fileBytes, "application/x-msdownload", fileName);
         }
 
         // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
+        [HttpGet ("{id}")]
+        public string Get (int id) {
             return "value";
         }
 
         // POST api/<controller>
         [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
+        public void Post ([FromBody] string value) { }
 
         // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        [HttpPut ("{id}")]
+        public void Put (int id, [FromBody] string value) { }
 
         // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        [HttpDelete ("{id}")]
+        public void Delete (int id) { }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,54 +1,46 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using System.Collections.Generic;
 
-namespace AngularAspNetCoreSignalR
-{
-    public class Startup
-    {
+namespace AngularAspNetCoreSignalR {
+    public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
-            {
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddCors (o => o.AddPolicy ("CorsPolicy", builder => {
                 builder
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .WithOrigins("http://localhost:4200");
+                    .AllowAnyMethod ()
+                    .AllowAnyHeader ()
+                    .WithOrigins ("*");
             }));
 
-            services.AddSignalR();
-            services.AddMvc();
+            services.AddSignalR ();
+            services.AddMvc ();
             //.SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-      //.ConfigureApiBehaviorOptions(options =>
-      //{
-      //    options.SuppressConsumesConstraintForFormFileParameters = true;
-      //    options.SuppressInferBindingSourcesForParameters = true;
-      //    options.SuppressModelStateInvalidFilter = true;
-      //    options.SuppressMapClientErrors = true;
+            //.ConfigureApiBehaviorOptions(options =>
+            //{
+            //    options.SuppressConsumesConstraintForFormFileParameters = true;
+            //    options.SuppressInferBindingSourcesForParameters = true;
+            //    options.SuppressModelStateInvalidFilter = true;
+            //    options.SuppressMapClientErrors = true;
 
-      //    options.ClientErrorMapping[404] = "https://httpstatuses.com/404";
-      //});
-       }
+            //    options.ClientErrorMapping[404] = "https://httpstatuses.com/404";
+            //});
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseCors("CorsPolicy");
+            app.UseCors ("CorsPolicy");
 
-            app.UseSignalR(routes =>
-            {
-                routes.MapHub<ChatHub>("chat");
+            app.UseSignalR (routes => {
+                routes.MapHub<ChatHub> ("chat");
             });
-
 
             //   List<string> angularRoutes = new List<string>(){
             //    "/home"
@@ -63,14 +55,17 @@ namespace AngularAspNetCoreSignalR
             //    await next();
             //});
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
+            app.UseDefaultFiles ();
+            app.UseStaticFiles ();
+            app.UseMvc (routes => {
+                routes.MapRoute (
                     name: "default",
                     template: "{controller}/{action}/{id?}");
             });
+            app.UseStaticFiles (new StaticFileOptions {
+                    ServeUnknownFileTypes = true, //allow unkown file types also to be served
+                        DefaultContentType = "Whatver you want eg: plain/text" //content type to returned if fileType is not known.
+                });
+            }
         }
     }
-}
